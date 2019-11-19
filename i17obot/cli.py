956c2a725041c3b8b94ebc79d250e81a0f54aab5
Marco Rougeth <marco@rougeth.com>
2019-11-19 22:08:37 +0000

    Create cli.py to download all strings locally

---
+++ i17obot/cli.py
@@ -0,0 +1,28 @@
+import asyncio
+import itertools
+import json
+import sys
+
+from transifex import strings_from_resource, transifex_api
+
+async def download_all_strings(file_to_save):
+    """ Download all strings in Transifex to JSON file
+    """
+    resources = await transifex_api(f"resources/")
+    resources = [resource["slug"] for resource in resources]
+    print("Resources", len(resources))
+
+    sema = asyncio.Semaphore(10)
+    async with sema:
+        strings = await asyncio.gather(
+            *[strings_from_resource(resource) for resource in resources]
+        )
+    strings = list(itertools.chain.from_iterable(strings))
+    print("Strings", len(resources))
+
+    with open(file_to_save, mode="w") as fp:
+        json.dump(strings, fp)
+
+
+if __name__ == "__main__":
+    asyncio.run(download_all_strings(sys.argv[1]))
--- i17obot/transifex.py
+++ i17obot/transifex.py
@@ -1,3 +1,5 @@
+import asyncio
+import logging
 import random
 from urllib.parse import quote, urljoin
 
@@ -9,13 +11,30 @@ TRANSIFEX_TOKEN = config("TRANSIFEX_TOKEN")
 TRANSIFEX_API = "https://www.transifex.com/api/2/project/python-newest/"
 
 
-@alru_cache()
-async def transifex_api(url):
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+async def transifex_api(url, data=None, retrying=False):
+    if retrying:
+        logger.info("retrying url=%s", url)
+
     auth = aiohttp.BasicAuth(login="api", password=TRANSIFEX_TOKEN)
     async with aiohttp.ClientSession(auth=auth) as session:
-        url = urljoin(TRANSIFEX_API, url)
-        async with session.get(url) as response:
-            return await response.json()
+        http_method = session.put if data else session.get
+        args = {"json": data} if data else {}
+
+        try:
+            async with http_method(urljoin(TRANSIFEX_API, url), **args) as response:
+                logger.info("url=%s, status_code=%s", url, response.status)
+                return await response.json()
+
+        except aiohttp.client_exceptions.ClientConnectorSSLError as e:
+            logger.error("url=%s, error=%s", url, e)
+            if not retrying:
+                await asyncio.sleep(2)
+                return await transifex_api(url, retrying=True)
+            raise
 
 
 async def random_resource():
@@ -24,16 +43,31 @@ async def random_resource():
     resources = filter(
         lambda r: r.split("--")[0] in ["bugs", "howto", "library"], resources
     )
-    return random.choice(list(resources))
+    resource = random.choice(list(resources))
+    logger.info("random_resource, resource=%s", resource)
+    return resource
+
+
+async def strings_from_resource(resource):
+    strings = await transifex_api(
+        f"resource/{resource}/translation/pt_BR/strings/?details"
+    )
+    logger.info(
+        "getting strings from resource, resource=%s, strings_found=%s",
+        resource,
+        len(strings),
+    )
+    for string in strings:
+        string["resource"] = resource
+
+    return strings
 
 
 async def random_string(resource=None, translated=None, reviewed=None, max_size=None):
     if not resource:
         resource = await random_resource()
 
-    strings = await transifex_api(
-        f"resource/{resource}/translation/pt_BR/strings/?details"
-    )
+    strings = await strings_from_resource(resource)
 
     if translated is not None:
         strings = filter(lambda s: bool(s["translation"]) == translated, strings)
@@ -62,4 +96,11 @@ def transifex_string_url(resource, key):
         "https://www.transifex.com/"
         f"python-doc/python-newest/translate/#pt_BR/{resource}/1"
         f"?q={quote(query_string)}"
+    )
+
+
+async def translate_string(resource, string_hash, translation):
+    await transifex_api(
+        f"resource/{resource}/translation/pt_BR/string/{string_hash}/",
+        data={"translation": translation},
     )
