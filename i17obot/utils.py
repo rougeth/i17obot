eb6815386761f84734c03c629a4a48b8169b8f12
Marco Rougeth <marco@rougeth.com>
2020-01-05 22:40:23 +0000

    Docs link to pt-br website

--- i17obot/utils.py
+++ i17obot/utils.py
@@ -1,4 +1,4 @@
 def docsurl(resource):
     docspath = "/".join(resource.split("--"))
     docspath = docspath.replace("_", ".").replace("..", "__")
-    return f"https://docs.python.org/{docspath}.html"
+    return f"https://docs.python.org/pt-br/3/{docspath}.html"
