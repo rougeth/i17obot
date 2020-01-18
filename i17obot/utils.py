3faa63a5c7eacfa62184cbd9fef51ead6e0058d5
Marco Rougeth <marco@rougeth.com>
2020-01-18 01:38:16 +0000

    Add corner cases when generating docs url

--- i17obot/utils.py
+++ i17obot/utils.py
@@ -1,4 +1,11 @@
 def docsurl(resource):
-    docspath = "/".join(resource.split("--"))
-    docspath = docspath.replace("_", ".").replace("..", "__")
+    corner_cases = {
+        "library--multiprocessing_shared_memory": "library/multiprocessing.shared_memory",
+    }
+
+    docspath = corner_cases.get(resource)
+    if not docspath:
+        docspath = "/".join(resource.split("--"))
+        docspath = docspath.replace("_", ".").replace("..", "__")
+
     return f"https://docs.python.org/pt-br/3/{docspath}.html"
