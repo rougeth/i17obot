56bba3060aa9c8248e80a8989a0fde5651cd1c68
Marco Rougeth <marco@rougeth.com>
2020-05-27 00:03:34 +0100

    Handle exceptions when reminder users

--- i17obot/reminder.py
+++ i17obot/reminder.py
@@ -2,15 +2,33 @@ import asyncio
 import logging
 from unittest.mock import Mock
 
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
+
 from aiogram import types
 from aiogram.utils.exceptions import BotBlocked
 
-from i17obot import bot
+from i17obot import bot, config
 from i17obot.database import get_users_with_reminder_on
 from i17obot.handlers.translate import translate
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+
+def setup_sentry():
+    if not config.SENTRY_DSN:
+        return
+
+    # All of this is already happening by default!
+    sentry_logging = LoggingIntegration(
+        level=logging.INFO,        # Capture info and above as breadcrumbs
+        event_level=logging.ERROR  # Send errors as events
+    )
+    sentry_sdk.init(
+        dsn=config.SENTRY_DSN,
+        integrations=[sentry_logging]
+    )
 
 
 async def reminder(user_id):
@@ -31,9 +49,14 @@ async def reminder(user_id):
 
 
 async def reminder_all_users():
+    setup_sentry()
     print("Running reminder_all_users task")
     users = await get_users_with_reminder_on()
     print("Users to reminde:", len(users))
     tasks = [reminder(user["id"]) for user in users]
     print("Users with reminter set on:", len(tasks))
-    await asyncio.gather(*tasks)
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+
+    for result in results:
+        if isinstance(result, Exception):
+            logger.exception("Error while reminding user. exception=%r", result)
